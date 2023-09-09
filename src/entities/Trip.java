6f707e646dd515e86999485a398d75babd6fffaf
FinnTD <132870548+FinnTD@users.noreply.github.com>
2023-09-09 16:20:47 +0700

    Added id attribute for the Trip class

--- src/entities/Trip.java
+++ src/entities/Trip.java
@@ -3,6 +3,7 @@ package entities;
 import java.util.Date;
 
 public class Trip {
+    private String id;
     private Vehicle trackingVehicle;
     private Date departureDate;
     private Date arrivalDate;
