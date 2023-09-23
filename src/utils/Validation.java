a2b55d719395888e9dbbb90ec3c6b8284118f1f3
TNQAn <127377415+TNQAn@users.noreply.github.com>
2023-09-23 16:52:28 +0700

    Time validation

--- src/utils/Validation.java
+++ src/utils/Validation.java
@@ -1,4 +1,24 @@
 package utils;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 
 public class Validation {
+
+
+    public class DateValidator {
+        private static final String DATE_FORMAT = "yyyy-MM-dd";
+
+        public static boolean isValidDate(String date) {
+            SimpleDateFormat sdf = new SimpleDateFormat(DATE_FORMAT);
+            sdf.setLenient(false);
+
+            try {
+                sdf.parse(date);
+                return true;
+            } catch (ParseException e) {
+                return false;
+            }
+        }
+    }
+
 }
